c744481b9fa24b47eccafc2803944e23904baf28
tanadon parosin <73661116+tanadonparosin@users.noreply.github.com>
2020-12-13 00:26:37 +0700

    Update Project_Timer_completely.py

--- Project_Timer_completely.py
+++ Project_Timer_completely.py
@@ -8,7 +8,7 @@ def timer():
     main_desktop = tk.Tk()
     main_desktop.title("Timer")
     main_desktop.geometry("600x500")
-    main_desktop.configure(bg = "Antiquewhite")
+    main_desktop.configure(bg = "pink")
 
     def timer_counter():
         global count
